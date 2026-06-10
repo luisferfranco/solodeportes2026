158dfd519a19d3988ea256f76e70220d197a928b
Luis F. Franco, PhD, MOU <lffrancoj@fertinal.com>
2026-06-10 09:46:48 -0600

    Cálculo de pronósticos por partido

--- resources/views/components/⚡pronostico-juego.blade.php
+++ resources/views/components/⚡pronostico-juego.blade.php
@@ -103,10 +103,13 @@ new class extends Component
       </div>
 
       @if (auth()->user()->id == 1)
-        <div class="flex items-center justify-between gap-1 font-bold text-base-content/50">
-          <div>{{ Number::format($neg) }}</div>
-          <div>{{ Number::format($emp) }}</div>
-          <div>{{ Number::format($pos) }}</div>
+        <div class="mt-2 px-2 py-1 bg-base-200">
+          <p class="text-center text-xs font-bold tracking-widest mb-2">Tendencias</p>
+          <div class="flex items-center justify-between gap-1 font-bold text-base-content/50">
+            <div>{{ Number::format($neg, precission: 2) }}%</div>
+            <div>{{ Number::format($emp, precission: 2) }}%</div>
+            <div>{{ Number::format($pos, precission: 2) }}%</div>
+          </div>
         </div>
       @endif
     </div>
