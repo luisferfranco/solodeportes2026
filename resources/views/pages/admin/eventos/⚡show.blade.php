2208edae25e23cc471b0a5de9b0353778eb87b65
Luis F. Franco, PhD, MOU <lffrancoj@fertinal.com>
2026-06-10 13:00:53 -0600

    feat: Información sobre un evento (admin)

--- resources/views/pages/admin/eventos/⚡show.blade.php
+++ resources/views/pages/admin/eventos/⚡show.blade.php
@@ -6,9 +6,21 @@ use Livewire\Component;
 new class extends Component
 {
   public Evento $evento;
+  public $participaciones;
+  public $headers;
 
   public function mount(Evento $evento) {
     $this->evento = $evento;
+
+    $this->participaciones = $evento->participaciones()
+      ->with('user')
+      ->get();
+    $this->headers = [
+      ['key' => 'id', 'value' => 'ID', 'class'=>"w-5"],
+      ['key' => 'nombre', 'value' => 'Nombre'],
+      ['key' => 'user.name', 'value' => 'Usuario'],
+      ['key' => 'created_at', 'value' => 'Fecha de Participación'],
+    ];
   }
 };
 ?>
@@ -17,4 +29,12 @@ new class extends Component
   <x-title :title="$evento->nombre" />
 
   <livewire:evento-info :evento="$evento" />
+
+  <x-card class="bg-base-100">
+    <x-table
+      :headers="$headers"
+      :rows="$participaciones"
+      :link=""
+      />
+  </x-card>
 </div>
