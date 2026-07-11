e650c7eded35732a78c15d5f7a7c2fa93a5a69c0
Luis Fernando Franco <luisferfranco@gmail.com>
2026-07-10 19:59:14 -0600

    Requerimientos de editor Markdown

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,13 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, viewport-fit=cover">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
+
   <title>{{ isset($title) ? $title.' - '.config('app.name') : config('app.name') }}</title>
 
   @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <link rel="stylesheet" href="https://unpkg.com/easymde/dist/easymde.min.css">
+  <script src="https://unpkg.com/easymde/dist/easymde.min.js"></script>
 </head>
 <body class="min-h-screen font-sans antialiased bg-base-200">
 
